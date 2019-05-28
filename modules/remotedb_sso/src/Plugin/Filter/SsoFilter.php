53ace2e8328913a320428c880e3e84e04c9b165a
MegaChriz <megachriz@hotmail.com>
2019-05-28 12:54:26 +0200

    Ported sso filter plugin.

---
+++ modules/remotedb_sso/src/Plugin/Filter/SsoFilter.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Drupal\remotedb_sso\Plugin\Filter;
+
+use Drupal\Core\Config\ConfigBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\remotedb\Exception\RemotedbException;
+use Drupal\remotedb_sso\TicketServiceInterface;
+use Drupal\remotedb_sso\UrlInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a filter to limit allowed HTML tags.
+ *
+ * @Filter(
+ *   id = "remotedb_sso",
+ *   title = @Translation("SSO Link filter"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
+ *   settings = {
+ *     "websites" = ""
+ *   }
+ * )
+ */
+class SsoFilter extends FilterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current active user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The remotedb_sso configuration.
+   *
+   * @var \Drupal\Core\Config\ConfigBase
+   */
+  protected $config;
+
+  /**
+   * The SSO url generator.
+   *
+   * @var \Drupal\remotedb_sso\UrlInterface
+   */
+  protected $urlGenerator;
+
+  /**
+   * The service for requesting tickets from the remote database.
+   *
+   * @var \Drupal\remotedb_sso\TicketServiceInterface|null
+   */
+  protected $ticketService;
+
+  /**
+   * Constructs a new Sso object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current active user.
+   * @param \Drupal\Core\Config\ConfigBase $config
+   *   The remotedb_sso configuration.
+   * @param \Drupal\remotedb_sso\UrlInterface $url_generator
+   *   The SSO url generator.
+   * @param \Drupal\remotedb_sso\TicketServiceInterface $ticket_service
+   *   (optional) The service for requesting tickets from the remote database.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountProxyInterface $current_user, ConfigBase $config, UrlInterface $url_generator, TicketServiceInterface $ticket_service = NULL) {
+    $this->currentUser = $current_user;
+    $this->config = $config;
+    $this->urlGenerator = $url_generator;
+    $this->ticketService = $ticket_service;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    try {
+      $ticket_service = $container->get('remotedb_sso.ticket');
+    }
+    catch (RemotedbException $e) {
+      // Ignore remotedb exceptions.
+      $ticket_service = NULL;
+    }
+
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('config.factory')->get('remotedb_sso.settings'),
+      $container->get('remotedb_sso.url'),
+      $ticket_service
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['websites'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Websites'),
+      '#description' => $this->t('Specify to which external websites an SSO link automatically must created, one on each line. Omit the http://, but include the subdomain if necessary, such as "www".') . ' ' . $this->t('Leave empty to use the defaults which can be set at @remotedb_sso_settings_url page.', [
+        '@remotedb_sso_settings_url' => Link::createFromRoute($this->t('SSO settings'), 'remotedb_sso.admin_settings_form')->toString(),
+      ]),
+      '#default_value' => $this->settings['websites'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    if (empty($this->ticketService)) {
+      // Bail out if the ticket service is not available.
+      return new FilterProcessResult($text);
+    }
+
+    // Do not rewrite links if the user is not logged in.
+    if ($this->currentUser->isAnonymous()) {
+      return new FilterProcessResult($text);
+    }
+
+    try {
+      if (!empty($this->settings['websites'])) {
+        $sites = $this->settings['websites'];
+        $sites = explode("\n", $sites);
+      }
+      else {
+        $sites = $this->config->get('websites');
+      }
+
+      foreach ($sites as $site) {
+        $text = $this->urlGenerator->createSsoGotoUrl($site, $text);
+      }
+    }
+    catch (RemotedbException $e) {
+      // Ignore any remote database exceptions.
+    }
+
+    return new FilterProcessResult($text);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    return $this->t('SSO links to certain external websites will be automatically created.');
+  }
+
+}
---
+++ modules/remotedb_sso/tests/src/Unit/Filter/SsoFilterTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Drupal\Tests\remotedb_sso\Unit\Filter;
+
+use Drupal\Core\Config\ConfigBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\filter\FilterProcessResult;
+use Drupal\remotedb_sso\Plugin\Filter\SsoFilter;
+use Drupal\remotedb_sso\TicketServiceInterface;
+use Drupal\remotedb_sso\UrlInterface;
+use Drupal\Tests\UnitTestCase;
+use Prophecy\Argument;
+
+/**
+ * Test if the SSO Filter works as expected.
+ *
+ * @coversDefaultClass \Drupal\remotedb_sso\Plugin\Filter\SsoFilter
+ * @group remotedb_sso
+ */
+class SsoFilterTest extends UnitTestCase {
+
+  /**
+   * The text to filter.
+   *
+   * @var string
+   */
+  const TEXT = 'At http://www.example.com there is a place at <a href="http://www.example2.com/subsite/place">.';
+
+  /**
+   * The current active user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $currentUser;
+
+  /**
+   * The remotedb_sso configuration.
+   *
+   * @var \Drupal\Core\Config\ConfigBase|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $config;
+
+  /**
+   * The SSO url generator.
+   *
+   * @var \Drupal\remotedb_sso\UrlInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $urlGenerator;
+
+  /**
+   * The service for requesting tickets from the remote database.
+   *
+   * @var \Drupal\remotedb_sso\TicketServiceInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $ticketService;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->currentUser = $this->prophesize(AccountProxyInterface::class);
+    $this->config = $this->prophesize(ConfigBase::class);
+    $this->urlGenerator = $this->prophesize(UrlInterface::class);
+    $this->ticketService = $this->prophesize(TicketServiceInterface::class);
+  }
+
+  /**
+   * Creates a new SSO filter.
+   *
+   * @param array $configuration
+   *   (optional) The filter's configuration.
+   */
+  protected function createFilter(array $configuration = []) {
+    $filter = new SsoFilter($configuration, 'remotedb_sso', ['provider' => 'test'], $this->currentUser->reveal(), $this->config->reveal(), $this->urlGenerator->reveal(), $this->ticketService->reveal());
+    $filter->setStringTranslation($this->getStringTranslationStub());
+
+    return $filter;
+  }
+
+  /**
+   * @covers ::process
+   */
+  public function testProcess() {
+    $this->currentUser->isAnonymous()->willReturn(FALSE);
+    $this->config->get()->shouldNotBeCalled();
+    $this->urlGenerator->createSsoGotoUrl(Argument::type('string'), Argument::type('string'))->will(function ($args) {
+      return str_replace($args[0], 'foo', $args[1]);
+    })->shouldBeCalledTimes(3);
+
+    $configuration['settings'] = [
+      'websites' => "www.example.com\nwww.example2.com/subsite\nwww.example3.com",
+    ];
+    $expected = new FilterProcessResult('At http://foo there is a place at <a href="http://foo/place">.');
+    $actual = $this->createFilter($configuration)->process(static::TEXT, 'und');
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Test processing using the remotedb_sso configuration.
+   *
+   * @covers ::process
+   */
+  public function testProcessWithSsoConfig() {
+    $this->currentUser->isAnonymous()->willReturn(FALSE);
+    $this->config->get('websites')->willReturn([
+      'www.example.com',
+      'www.example2.com/subsite',
+      'www.example3.com',
+    ]);
+    $this->urlGenerator->createSsoGotoUrl(Argument::type('string'), Argument::type('string'))->will(function ($args) {
+      return str_replace($args[0], 'foo', $args[1]);
+    })->shouldBeCalledTimes(3);
+
+    $expected = new FilterProcessResult('At http://foo there is a place at <a href="http://foo/place">.');
+    $actual = $this->createFilter()->process(static::TEXT, 'und');
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Tests that text does not change when the current user is anonymous.
+   *
+   * @covers ::process
+   */
+  public function testProcessWithAnonymousUser() {
+    $this->currentUser->isAnonymous()->willReturn(TRUE);
+    $this->config->get()->shouldNotBeCalled();
+    $this->urlGenerator->createSsoGotoUrl()->shouldNotBeCalled();
+
+    $expected = new FilterProcessResult(static::TEXT);
+    $actual = $this->createFilter()->process(static::TEXT, 'und');
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Tests that text does not change when the ticket service is not available.
+   *
+   * @covers ::process
+   */
+  public function testProcessWithoutTicketService() {
+    $this->currentUser->isAnonymous()->shouldNotBeCalled();
+    $this->config->get()->shouldNotBeCalled();
+    $this->urlGenerator->createSsoGotoUrl()->shouldNotBeCalled();
+
+    $filter = new SsoFilter([], 'remotedb_sso', ['provider' => 'test'], $this->currentUser->reveal(), $this->config->reveal(), $this->urlGenerator->reveal(), NULL);
+
+    $expected = new FilterProcessResult(static::TEXT);
+    $actual = $filter->process(static::TEXT, 'und');
+    $this->assertEquals($expected, $actual);
+  }
+
+}
